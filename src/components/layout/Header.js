6bd3bbdbc4e3c55d28b34534c7651f4cd614865b
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-27 14:31:40 -0400

    feat(header): Se crea header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const Header = () => (
+    <Fragment>
+        <header className="barra">
+            <div className="contenedor">
+                <h1>CRM - Administrador de Clientes</h1>
+            </div>
+        </header>   
+    </Fragment>
+)
+
+export default Header
