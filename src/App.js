2a0c17f3338839623c303557beccc74f854d3d6b
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-27 15:05:15 -0400

    feat(routing):Se crea enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React, {Fragment} from 'react'
 
+// Routing
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 // Layout
 import Header from './components/layout/Header';
 import Navegacion from './components/layout/Navegacion';
 
+// Componentes
+import Clientes from './components/clientes/Clientes';
+import Productos from './components/productos/Productos';
+import Pedidos from './components/pedidos/Pedidos';
+
 
 function App() {
   return (
+  <BrowserRouter>
     <Fragment>
-      <Header />
-      <div class="grid contenedor contenido-principal">
-        <Navegacion />
-        <main class="caja-contenido col-9">
-          {/* TODO : Routing a los diferentes Componentes */}
-        </main>
-      </div>
-    </Fragment>
+        <Header />
+        <div class="grid contenedor contenido-principal">
+          <Navegacion />
+          <main class="caja-contenido col-9">
+            <Routes>
+              <Route path="/" element={<Clientes/>}/>
+              <Route path="/productos" element={<Productos/>}/>
+              <Route path="/pedidos" element={<Pedidos/>}/>
+            </Routes>
+          </main>
+        </div>
+      </Fragment>
+  </BrowserRouter>
   )
 }
 export default App;
