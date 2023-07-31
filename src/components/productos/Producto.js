6541cdc5ea5cfef1bed3ab82b33fd3b63a8e4511
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-31 10:33:01 -0400

    feat(productos): Vista de productos

---
+++ src/components/productos/Producto.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import clienteAxios from '../../config/axios'
+
+function Producto({producto}) {
+    const { _id,nombre,precio, imagen } = producto
+
+    // Eliminar producto
+    const eliminarProducto = idProducto => {
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Un producto eliminado no se puede recuperar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // LLamado a axios
+                clienteAxios.delete(`/productos/${idProducto}`)
+                    .then(res => {
+                        if (res.status === 200){
+                            Swal.fire(
+                                'Eliminado!',
+                                res.data.mensaje,
+                                'success'
+                            )
+                        }
+                })
+            }
+        })
+    }
+    return (
+        <li className="producto">
+            <div className="info-producto">
+                <p className="nombre">{nombre}</p>
+                <p className="precio">$ {precio} </p>
+                { imagen ? (
+                    <img src={`http://localhost:5000/${imagen}`} alt='hola'/>
+                ) : null
+                }
+            </div>
+            <div className="acciones">
+                <Link to={`/productos/editar/${_id}`} className="btn btn-azul">
+                    <i className="fas fa-pen-alt"></i>
+                    Editar Producto
+                </Link>
+
+                <button 
+                    type="button" 
+                    className="btn btn-rojo btn-eliminar"
+                    onClick={() => eliminarProducto(_id)}
+                >
+                    <i className="fas fa-times"></i>
+                    Eliminar Cliente
+                </button>
+            </div>
+        </li>
+    )
+}
+
+export default Producto
